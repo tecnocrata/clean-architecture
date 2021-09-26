b361a2e25f8faa69a184691fc193b1c6c3ca7a3b
enrique.ortuno <enrique@ortuno.net>
2021-09-25 23:32:56 -0400

    DbContext and MFE entity added

---
+++ src/VersioningService.Infrastructure/Context/VersioningDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using VersioningService.Infrastructure.Entities;
+
+namespace VersioningService.Infrastructure.Context
+{
+    public class VersioningDbContext: DbContext
+    {
+        public VersioningDbContext(DbContextOptions<VersioningDbContext> options): base(options)
+        {
+        }
+
+        public virtual DbSet<MicrofrontEnd> MicrofronEnds { get; set; }
+    }
+}
---
+++ src/VersioningService.Infrastructure/Entities/MicrofrontEnd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VersioningService.Infrastructure.Entities
+{
+    public class MicrofrontEnd
+    {
+        public MicrofrontEnd()
+        {
+        }
+
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Version { get; set; }
+    }
+}
