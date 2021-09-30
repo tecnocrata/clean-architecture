bb469f78081af86395f94db82da1f2de53d49252
enrique.ortuno <enrique@ortuno.net>
2021-09-29 20:55:15 -0400

    Automapper configured

--- src/VersioningService.Infrastructure/Context/VersioningDbContext.cs
+++ src/VersioningService.Infrastructure/Context/VersioningDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using VersioningService.Infrastructure.Entities;
 
@@ -8,8 +9,22 @@ namespace VersioningService.Infrastructure.Context
     {
         public VersioningDbContext(DbContextOptions<VersioningDbContext> options): base(options)
         {
+            SeedData();
         }
 
         public virtual DbSet<MicrofrontEnd> MicrofronEnds { get; set; }
+
+        private void SeedData()
+        {
+            var mfes = new List<MicrofrontEnd>()
+            {
+                new MicrofrontEnd(){Id= Guid.NewGuid(), Name="hours-editor-vue", Url="http://localhost:6002/remoteEntry.js", Version="1.0.1", CreatedBy="Enrique", PublishedAt= DateTime.Now},
+                new MicrofrontEnd(){Id= Guid.NewGuid(), Name="snippet-editor-angular11", Url="http://localhost:4203/remoteEntry.js", Version="1.1.0", CreatedBy="Enrique", PublishedAt= DateTime.Now},
+                new MicrofrontEnd(){Id= Guid.NewGuid(), Name="snippet-editor-angular11", Url="http://localhost:42031/remoteEntry.js", Version="2.0.0", CreatedBy="Enrique", PublishedAt= DateTime.Now},
+                new MicrofrontEnd(){Id= Guid.NewGuid(), Name="hours-editor-vue", Url="http://localhost:60021/remoteEntry.js", Version="1.2.0", CreatedBy="Enrique", PublishedAt= DateTime.Now}
+            };
+            MicrofronEnds.AddRange(mfes);
+            SaveChanges();
+        }
     }
 }
--- src/VersioningService/Startup.cs
+++ src/VersioningService/Startup.cs
@@ -27,6 +27,8 @@ namespace VersioningService
         {
 
             services.AddControllers();
+
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
