f820d3c3fce91bfcf7b8ac4b54bd8b5135c3586e
enrique.ortuno <enrique@ortuno.net>
2021-10-08 21:58:50 -0400

    appsettings.cs modified

---
+++ src/VersioningService/ApiConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VersioningService
+{
+    public class ApiConstants
+    {
+        public const string ServiceName = "MicrofrontEndService";
+        public const string FriendlyServiceName = "Microfront End Service";
+
+    }
+}
--- src/VersioningService/AppSettings.cs
+++ src/VersioningService/AppSettings.cs
@@ -6,5 +6,8 @@ namespace VersioningService.AppSettings
         public AppSettings()
         {
         }
+
+        public string KeyVaultName { get; set; }
+        public bool ByPassKeyVault { get; set; }
     }
 }
