9f983942f5b7d3be20f634fd0b5ff47a25690155
enrique.ortuno <enrique@ortuno.net>
2021-10-10 22:27:42 -0400

    logger abstraction added to service layer

--- src/VersioningService.Core/Services/MicrofrontEndService.cs
+++ src/VersioningService.Core/Services/MicrofrontEndService.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using VersioningService.Core.Interfaces.Repositories;
 using VersioningService.Core.Interfaces.Services;
 using VersioningService.Core.Models;
 
 namespace VersioningService.Core.Services
 {
-    public class MicrofrontEndService: IMicrofrontEndService
+    public class MicrofrontEndService : IMicrofrontEndService
     {
         private readonly IMicrofrontEndRepository _mfeRepository;
+        private readonly ILogger<MicrofrontEndService> logger;
 
-        public MicrofrontEndService(IMicrofrontEndRepository mfeRepository)
+        public MicrofrontEndService(IMicrofrontEndRepository mfeRepository, ILogger<MicrofrontEndService> logger)
         {
-            _mfeRepository = mfeRepository?? throw new ArgumentNullException(nameof(mfeRepository));
+            _mfeRepository = mfeRepository ?? throw new ArgumentNullException(nameof(mfeRepository));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public Task<bool> CreateMicrofronEnd(MicrofronEnd mfe)
