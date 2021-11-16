b1f8d512386e44cabf4d3b288b9dc54e90df0adf
enrique.ortuno <enrique@ortuno.net>
2021-11-15 20:03:34 -0400

    KeyVault fake implemented and added

---
+++ src/VersioningService/KeyVaultCache.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace VersioningService
+{
+    public class KeyVaultCache
+    {
+        public static string BaseUri { get; set; }
+
+        private static Dictionary<string, string> SecretsCache = new Dictionary<string, string>();
+        /// <summary>
+        /// Also known as GetAzureKeyValueSecrets
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="config"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void GetVaultSecrets(HostBuilderContext context, IConfigurationBuilder config)
+        {
+            //throw new NotImplementedException();
+            var builtConfig = config.Build();
+            var keyVaultName = builtConfig[$"AppSettings:KeyVaultName"];
+            // BaseUri = $"https://{keyVaultName}.vault.azure.net";
+            // var secretClient = new SecretClient(new Uri(BaseUri), new DefaultAzureCredential());
+            // config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+        }
+
+        public static string GetCachedSecret(string secretName)
+        {
+            if (!SecretsCache.Contains(secretName))
+            {
+                var secret = KeyVaultClientGetSecret(secretName);
+                SecretsCache.Add(secretName, secret);
+            }
+        }
+
+        private static string KeyVaultClientGetSecret(string secretName)
+        {
+            // This method fakes the real implementation that would retrieve from AWS/Azure
+            switch (secretName)
+            {
+                case "versioningdb": return "Server=localhost;Database=versioningdb;Integrated Security=true;";
+                default: return "";
+            }
+        }
+    }
+
+    public static class GetSecret
+    {
+        public static string VersioningConnectionString(string env) =>{
+            // await KeyVaultCache
+        }
+}
+
+}
